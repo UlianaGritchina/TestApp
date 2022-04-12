eb281d8d3d7f517a774b7caddb49d920d9a52654
UlianaGritchina <ugritchina@icloud.com>
2022-04-12 18:54:11 +0300

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Ульяна Гритчина on 12.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
